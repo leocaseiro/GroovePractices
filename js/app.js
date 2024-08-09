5f3d72c83f118ba29fa9a793d32a4b46e6d5a258
Leo Caseiro <leobok@gmail.com>
2024-08-10 08:18:25 +1000

    chore(TestData): commet test data

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 import { initDB } from './db.js';
 import { loadGrooves } from './ui.js';
 import { initializeSearch, populateAuthorFilter } from './search.js';
-import { populateTestData } from './testData.js';
+// import { populateTestData } from './testData.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initDB().then(() => {
         initializeSearch();
         loadGrooves();
         populateAuthorFilter();
-        populateTestData(50); // This will add 50 test grooves if the database is empty
+        // populateTestData(50); // This will add 50 test grooves if the database is empty
 
     }).catch(error => {
         console.error('Error initializing app:', error);
